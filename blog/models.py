9e0f42b1cc11a79222b580a1f62503b3cd325062
jomulagy <jomulagy988@gmail.com>
2023-03-21 15:51:08 +0900

    블로그 목록 뷰를 만들었다네

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     content = models.TextField()
     
     created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
     #author:
 
     def __str__(self):
